52cb733f7dd4ffbe4dea5d264b9e20e6a28cdd7d
benjamin-23 <kitongabenja34@gmail.com>
2023-02-08 12:23:41 +0300

    Drawer list

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,64 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  static const appTitle = "Happiness salon";
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Happiness Salon',
+      title: appTitle,
       theme: ThemeData(
           primaryColor: Colors.lightBlue[800],
           fontFamily: 'Geogia',
           brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
-      home: const MyMenu(),
+      home: const MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text("My page"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text("Drawer Header"),
+            ),
+            ListTile(
+              title: const Text("item 1"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text("Item 2"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
